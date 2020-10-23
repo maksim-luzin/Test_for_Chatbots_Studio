8e05f95ca57f93c5fdc5c10ac20c06996642bb3e
maksim-luzin <maksim-luzin@users.noreply.github.com>
2020-10-23 19:50:27 +0300

    Add user controller.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,14 +2,17 @@ import { Router } from 'express';
 import * as authController from '../controllers/authController';
 import * as userController from '../controllers/userController';
 
+import authenticationMiddleware from '../middlewares/authenticationMiddleware';
+import registrationMiddleware from '../middlewares/registrationMiddleware';
+
 const router = Router();
 
 // user added to the request (req.user) in a strategy, see passport config
 router
-  .post('/login', (req, res, next) => authController.login(req.user)
+  .post('/login', authenticationMiddleware, (req, res, next) => authController.login(req.user)
     .then(data => res.send(data))
     .catch(next))
-  .post('/register', (req, res, next) => authController.register(req.user)
+  .post('/register', registrationMiddleware, (req, res, next) => authController.register(req.user)
     .then(data => res.send(data))
     .catch(next))
   .get('/user', (req, res, next) => userController.getUserById(req.user.id)
